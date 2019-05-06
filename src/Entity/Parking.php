8c8a66d9bf6e195be18e81f2fb9030ddd60ce98f
swve <bdrswv@gmail.com>
2019-05-06 15:32:42 +0200

    [ ADD | MOD ] Parking ent , Place ent

---
+++ src/Entity/Parking.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ParkingRepository")
+ */
+class Parking
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\place", inversedBy="parkings")
+     */
+    private $place;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getPlace(): ?place
+    {
+        return $this->place;
+    }
+
+    public function setPlace(?place $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
--- src/Entity/Place.php
+++ src/Entity/Place.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -45,6 +47,16 @@ class Place
      * @ORM\Column(type="string", length=255)
      */
     private $position;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Parking", mappedBy="place")
+     */
+    private $parkings;
+
+    public function __construct()
+    {
+        $this->parkings = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -119,6 +131,37 @@ class Place
     public function setPosition(string $position): self
     {
         $this->position = $position;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Parking[]
+     */
+    public function getParkings(): Collection
+    {
+        return $this->parkings;
+    }
+
+    public function addParking(Parking $parking): self
+    {
+        if (!$this->parkings->contains($parking)) {
+            $this->parkings[] = $parking;
+            $parking->setPlace($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParking(Parking $parking): self
+    {
+        if ($this->parkings->contains($parking)) {
+            $this->parkings->removeElement($parking);
+            // set the owning side to null (unless already changed)
+            if ($parking->getPlace() === $this) {
+                $parking->setPlace(null);
+            }
+        }
 
         return $this;
     }
