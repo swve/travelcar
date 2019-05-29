3958e7db349443919f39be79b31ba7580a7d6d63
Gabriel Bugeaud <gabriel@seizeheures.fr>
2019-05-29 08:57:30 +0200

    Add ProfileType form type

---
+++ src/Form/ProfileType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProfileType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname', TextType::class, [
+                'required' => true,
+                'label' => 'First Name'
+            ])
+            ->add('lastname', TextType::class, [
+                'required' => true,
+                'label' => 'Last Name'
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label' => 'Email Address'
+            ])
+            ->add('img')
+            ->add('bio', TextareaType::class, [
+                'required' => false,
+                'label' => 'Describe yourself in a few sentences'
+            ])
+            ->add('username', TextType::class, [
+                'required' => false,
+                'label' => 'Set a nickname'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
