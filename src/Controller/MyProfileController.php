7c63eabf94684291ba65c608bdc0d38cab6f8288
Gabriel Bugeaud <gabriel@seizeheures.fr>
2019-05-29 09:24:04 +0200

    Add edit method to MyProfileController Tweak profile page

--- src/Controller/MyProfileController.php
+++ src/Controller/MyProfileController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class MyProfileController
+ * @package App\Controller
+ * @Route("/my/profile")
+ * @IsGranted("ROLE_USER")
+ */
 class MyProfileController extends AbstractController
 {
     /**
-     * @Route("/my/profile", name="my_profile")
+     * @Route("/", name="my_profile")
      */
     public function index()
     {
         return $this->render('my_profile/index.html.twig', [
-            'controller_name' => 'MyProfileController',
+            'user' => $this->getUser(),
         ]);
+    }
+
+    /**
+     * @Route("/edit", name="profile_edit")
+     */
+    public function edit()
+    {
+        //TODO: implement method stub
     }
 }
