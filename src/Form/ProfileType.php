62f2aca4e4783ff975d683189e64b8558d69fd9b
Gabriel Bugeaud <gabriel@seizeheures.fr>
2019-06-16 18:57:46 +0200

    Add avatar uploading for users

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -9,6 +9,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProfileType extends AbstractType
 {
@@ -27,7 +28,17 @@ class ProfileType extends AbstractType
                 'required' => true,
                 'label' => 'Email Address'
             ])
-            ->add('img')
+            ->add('imageFile', VichImageType::class, [
+                'label' => 'Avatar',
+                'required' => false,
+                'allow_delete' => true,
+                'download_uri' => false,
+                'image_uri' => false,
+                'delete_label' => 'Delete current avatar',
+                'attr' => [
+                    'class' => 'file-input',
+                ],
+            ])
             ->add('bio', TextareaType::class, [
                 'required' => false,
                 'label' => 'Describe yourself in a few sentences'
